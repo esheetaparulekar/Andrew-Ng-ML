726b62b4e448aaf4211fe89299d6f0e22ce7fd1f
esheetaparulekar <esheeta.parulekar@cumminscollege.in>
2020-05-30 11:44:13 +0530

    Add files via upload

--- plotData.m
+++ plotData.m
@@ -1,31 +1,34 @@
-function plotData(x, y)
-%PLOTDATA Plots the data points x and y into a new figure 
-%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
-%   population and profit.
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
-figure; % open a new figure window
+% Create New Figure
+figure; hold on;
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Plot the training data into a figure using the 
-%               "figure" and "plot" commands. Set the axes labels using
-%               the "xlabel" and "ylabel" commands. Assume the 
-%               population and revenue data have been passed in
-%               as the x and y arguments of this function.
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
 %
-% Hint: You can use the 'rx' option with plot to have the markers
-%       appear as red crosses. Furthermore, you can make the
-%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
-x = [-5 : 2 : 25]
-y = [-5 : 2 : 25]
-plot(x, y ,'rx','MarkerSize',5);
-xlabel('Population');
-ylabel('Revenue');
-title('My Plot');
-%axes("tic","labelx","labely");
+%X=[0:2:30];
+%y=[0:2:30];
+%plot(X,y);
 
 
 
 
-% ============================================================
+positive = find(y==1);
+negative = find(y==0);
+
+plot(X(positive, :), 'k+');
+plot(X(negative, :), 'ko');
+
+
+% =========================================================================
+
+
+
+hold off;
 
 end
